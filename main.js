4de69b1959f25b8a5c1af0817846854597a2b68f
Gui-Danni-Cmd <gumacielcoelho@gmail.com>
2023-03-15 22:15:30 -0300

    Indios

---
+++ main.js
@@ -0,0 +1,127 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <header>
+  <div class="container">
+    <img
+      
+    />
+    <div class="menu-section">
+      <div class="menu-toggle">
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#inic">Pataxó</a>
+          </li>
+          <li>
+            <a href="#ling">Língua</a>
+          </li>
+          <li>
+            <a href="#font">Fonte de renda</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  </header>
+  <div class="conteiner">
+  <div class="text">
+  <div class="couterbox">
+  <div id="inic"></div>
+    <img src="./public/pataxo_foto2.jpg"/>
+    <div class="inicio"><h1>Pataxó</h1>
+        <strong>Localização</strong> Os índios Pataxós, hoje em dia estão espalhados pela região sudeste e nordeste, mais especificamente em Minas Gerais e no extremo sul da Bahia. Sendo sua terra natal a Bahia. As aldeias do extremo sul da Bahia ficam localizadas em 36 aldeias distribuídas em seis Terras Indígenas sendo elas: Águas Belas, Aldeia Velha, Barra Velha, Imbiriba, Coroa Vermelha e Mata Medonha; Situadas nos municípios de Santa Cruz Cabrália, Porto Seguro, Itamaraju e Prado. Já no estado de Minas a situação é diferente sendo que das 7 comunidades viventes no estado 4 estão localizadas na Terra Indígena Fazenda Guarani, município de Carmésia; Muã Mimatxi, em um imóvel cedido à Funai(Fundação Nacional do Índio) pelo Serviço de Patrimônio da União, no município de Itapecerica; Esta migração para o estado de Minas Gerais ocorreu após o crime conhecido como fogo de 51, na Bahia, onde casas de uma comunidade índigena foram queimadas.
+      </div>
+  </div>
+  <div id="ling"class="couterbox">
+        <div class="inicio"><h1>Língua</h1>
+      
+          Os Pataxós atualmente falam a língua portuguesa, porém à hoje em dia uma tentativa de buscar os conhecimentos da sua língua materna que é o Patxohã - “Língua de Guerreiro”, que é uma língua do tronco Macro-Jê e da família linguística Maxakali. Essa busca ainda vem ocorrendo e é um incentivo de algumas aldeias para os indígenas voltarem a falar suas línguas maternas. Algumas escolas pataxós já estão adotando novos métodos de ensinar sua língua materna. “O ensino de Patxohã não se restringe ao léxico da língua, mas compreende um amplo conjunto de informações, tais como danças e canções indígenas; os processos históricos vivenciados pelos povos indígenas, particularmente aqueles estabelecidos no extremo-sul da Bahia; e a identidade indígena no presente” (Bomfim, 2012, p. 59).
+        </div>
+        <img src="./public/indio1.jpg"/>
+
+      </div>
+      <div class="couterbox">
+      <img src="./public/indio3.jpg"/>
+    <div id="font" class="inicio"><h1>Fonte de renda</h1>
+      As tribos Pataxós atualmente estão muito bem inseridas na sociedade atual, e ao contrário do que muitos pensam elas convivem muito bem com a população nos centros urbanos. Muitas delas tiram um proveito da curiosidade da população para ter uma renda. Muitas atualmente estão apostando no etno-turismo, onde visitantes podem participar de atividades características da tribo, aprender sobre suas lendas, rituais e histórias tradicionais. Além disso, temos também a confecção de artesanato vendido normalmente em feiras. Algumas tribos ainda fazem a plantação de cacau e criam gados para vender.
+    </div>
+    
+    </div>
+    <div class="couterbox">
+      <div class="inicio"><h1>Número de Habitantes</h1>
+  
+        Os dados do SIASI registram, para 2010, 11.436 habitantes (sendo 5.839 homens e 5.597 mulheres) distribuídos pelas aldeias Barra Velha, Aldeia Velha, Boca da Mata, Meio da Mata, Imbiriba, localizadas em Porto Seguro; Pé do Monte, Trevo do Parque, Guaxuma, Corumbauzinho e Aldeia Nova, estabelecidas em Itamaraju; Coroa Vermelha e Mata Medonha, em Santa Cruz de Cabrália; e, por fim, Águas Belas, Craveiro, Tauá, Tibá, Córrego do Ouro, Cahy e Alegria Nova no Prado, totalizando 19 aldeias. 
+      </div>
+    <img src="./public/indio4.jpg"/>
+  </div>
+
+  <div class="couterbox">
+    <img src="./public/indio5.jpg"/>  
+      <div class="inicio"><h1>Costumes</h1>
+        Como a maioria das tribos indígenas, o povo Pataxós possui inúmeras tradições, envolvendo diversos costumes:
+      </div>
+  </div>
+  <div class="couterbox">
+    <div class="inicio"><h1>TOHÉ</h1>
+      Nos dias de rituais, reúnem todos os índios para dançarem o tohé, que nada mais é do que uma forma de oração coletiva, podendo ser celebrado em momentos tristes e alegres. Unindo assim, idosos e crianças, homens e mulheres.
+    </div>
+    <img src="./public/indio6.jpg"/>  
+  </div>
+  <div class="couterbox">
+  <img src="./public/indio7.jpg"/>  
+  <div class="inicio"><h1>AWÊ</h1>
+   O Awê significa o amor, a união e a espiritualidade com a natureza. É o mais antigo ritual, ou seja, sempre existiu, ninguém sabe informar quando começou. Englobam coreografias variadas, cada pessoa dança com um sentido determinado.
+  </div>
+  </div>
+  <div class="couterbox">
+    <div class="inicio"><h1>RITUAL DA CHUVA</h1>
+     Antigamente, os índios mais velhos faziam esse ritual quando estavam precisando da chuva. Eles    faziam amontoados de galhos e folhas e, colocavam fogo. Assim, com a fumaça virando nuvem, caía à  chuva. Se fizessem de manhã, a chuva cairia de tarde. Por fim, eles faziam um canto de agradecimento. 
+    </div>
+  <img src="./public/indio8.jpg"/>  
+  </div>
+
+  <div class="couterbox">
+  <img src="./public/indio12.jpg"/>  
+  <div class="inicio"><h1>NAMORO E CASAMENTO</h1>
+    O namoro pataxó é bem discreto, quando um se interessava pelo outro, eles ficavam se jogando pedrinhas, isso é um sinal que eles já estavam namorando. Quando o índio resolve pedir sua namorada em casamento, ele entrega uma flor, se ela aceitar é porque a resposta foi “sim”.
+    </div>
+  </div>
+
+  <div class="couterbox">
+  <div class="inicio"><h1>ARSGWAKSÁ</h1>
+  É uma festa comemorativa do aniversário do Projeto Jaqueira, que foi o primeiro projeto implantado em uma aldeia indígena, os índios desta reserva estão envolvidos em um projeto de responsabilidade social. Essa festa inclui apresentações e provas físicas.
+  </div>
+  <img src="./public/indio9.jpg"/>  
+  </div>
+
+  <div class="couterbox">
+  <img src="./public/indio10.jpg"/>  
+  <div class="inicio"><h1>FOLIA DE REIS</h1>
+    Na tribo Pataxó, é muito comum o relato de folia e esmola do Divino Espírito Santo. Um grupo de foliões chega carregando uma bandeira e se dirige à capela, depois de recolher as esmolas. A reza noturna atrai a população local. 
+    </div>
+  </div>
+
+  <div class="couterbox">
+  <div class="inicio"><h1>Curiosidades</h1>
+  Os índios Pataxós foram percebidos como tribos indígenas brasileiras logo após a chegada dos portugueses ao Brasil, no ano de 1.500, junto à tribo Tupi-guarani. Pelo norte do estado do Espírito Santo até Ilhéus na Bahia era possível encontrar grande número desses índios. <br><br>
+  A Reserva da Jaqueira, que se estende por 800 hectares de floresta atlântica de Porto Seguro (BA), abriga e resgata os costumes da tribo Pataxó. No local, trabalham 50 índios que desenvolvem atividades ecológicas e culturais.
+  Atualmente, a maioria dos pataxós mora na vila da Coroa Vermelha, também localizada em Porto Seguro. 
+  </div>
+  <img src="./public/indio11.jpg"/>  
+  </div>
+  <div class="bar">
+    <footer>
+      Todos os direitos reservados a os alunos da funec riacho.
+    </footer>
+  </div>
+
+  </div>
+`
+
+
